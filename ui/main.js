ff6c991e0177a0412dbd2bfa8cda66271509a622
lakshmi2508 <30799381+lakshmi2508@users.noreply.github.com>
2017-08-16 11:45:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,7 @@ element.innerHTML="New Text";
 var img=document.getElementById("madi");
 var marginLeft=0;
 function moveright(){
+    img.style.marginleft=marginLeft+"px";
     if(marginLeft<=50){
     marginLeft=marginLeft+10;
     img.style.marginleft=marginLeft+"px";
