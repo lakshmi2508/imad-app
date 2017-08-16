965b898be585d639950125d210995354b4974d4c
lakshmi2508 <30799381+lakshmi2508@users.noreply.github.com>
2017-08-16 11:17:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,4 +3,6 @@ var element=document.getElementById("main-text");
 element.innerHTML="New Text";
 
 var img=document.getElementById("madi");
+img.OnClick=function(){
 img.style.marginLeft="100px";
+}
