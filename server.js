5492198e3d35c41bdee286be468c4c9457a7acfa
lakshmi2508 <30799381+lakshmi2508@users.noreply.github.com>
2017-08-16 21:45:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/', function (req, res) {
 
 var names=[];
 app.get('/submit-name',function(req,res){
-    var name=request.query.name;
+    var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
