c54872f5eb4662421465b6abac478da7a92eb4a9
lakshmi2508 <30799381+lakshmi2508@users.noreply.github.com>
2017-08-19 19:18:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config={
     database:'lakshmi2508',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:'db-lakshmi2508-46808'
+    password:process.env.DB_PASSWORD
 };
 
 var app = express();
