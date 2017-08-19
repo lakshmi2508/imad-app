11c1766d2e57a4eca80715ee33e7cde618472d5b
lakshmi2508 <30799381+lakshmi2508@users.noreply.github.com>
2017-08-19 21:26:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,8 +97,13 @@ app.get('/articles/:articleName',function(req,res){
            res.status(500).send(err.toString());
        } 
        else{
+           if(result.rows.length===0){
+               res.status(404).send("No article found");
+           }
+           else{
        var articledata=result.rows[0];
        res.send(createTemplate(articledata));
+           }
        }
     });
 });
