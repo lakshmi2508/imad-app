31c517cf5dd9db312680c9b490fd1218a08b1f51
lakshmi2508 <30799381+lakshmi2508@users.noreply.github.com>
2017-08-21 21:44:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ var config={
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-app.use(session({
+/*app.use(session({
     secret:'someRandomSecretValue',
     resave:false,
     saveUninitialized:true,
     cookie:{maxAge:1000 * 60 * 60 * 24 *30}
-}));
+}));*/
 
 
 /*var articles={
@@ -119,7 +119,7 @@ app.post("/login",function(req,res){
                var salt=dbString.split('$')[2];
                var newpassword=hash(password,salt);
                if(newpassword===dbString){
-                   req.session.auth={userId:result.rows[0].id};
+                   //req.session.auth={userId:result.rows[0].id};
                    res.send("Login done!");
                    
                }
