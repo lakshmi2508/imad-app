6f75779874f2308ac2e3a4889a0099105e258937
lakshmi2508 <30799381+lakshmi2508@users.noreply.github.com>
2017-08-21 22:36:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json());
 app.use(session({
     secret: 'someRandomSecretValue',
     saveUninitialized: false,
+    resave: false,
     cookie:{maxAge:1000 * 60 * 60 * 24 *30}
 }));
 
